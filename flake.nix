84a7540b18fec949057e736c514a154412b26df0
Robert Hensing <robert@roberthensing.nl>
2022-11-06 17:33:49 +0100

    Update hercules-ci-effects

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
     pre-commit-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
-    hercules-ci-effects.url = "hercules-ci-effects/netlifyDeploy-extraDeployArgs";
   };
 
   outputs = inputs@{ self, flake-parts, hercules-ci-effects, ... }:
